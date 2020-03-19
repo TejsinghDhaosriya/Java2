b89606363cbd050a7cdbdf3aff7e51f7977769bc
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-03-19 10:53:25 +0530

    InstanceControlFlow

---
+++ OOPS/static/InstanceControlFlow.java
@@ -0,0 +1,26 @@
+class InstanceControlFlow{
+
+int i=10;
+{
+m1();
+System.out.println("first instance block");
+}
+InstanceControlFlow()
+{
+
+System.out.println("Parent classs Constructor");
+}
+public static void main(String[] args)
+{
+    InstanceControlFlow ICF=new InstanceControlFlow();
+    System.out.println("main method");
+}
+public void m1()
+  {
+      System.out.println(j);
+  }
+  {
+      System.out.println("second instance block");
+  }
+  int j=20;
+}
