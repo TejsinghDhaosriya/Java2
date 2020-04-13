313be22626330563d1bdef40b9e73e6b3d6c1127
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-04-13 20:25:16 +0530

    Serializating dog and cat

--- Serialization/Serializable_2_objects.java
+++ Serialization/Serializable_2_objects.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.Scanner;
 
-class Cat{
+class Cat implements Serializable{
   int id =22;
   String name = "cat";
 
 }
-class Dog{
+class Dog implements Serializable{
   int id = 34;
   String name = "dog";
 
@@ -23,7 +23,15 @@ class Serializable_2_objects
        ObjectOutputStream oos = new ObjectOutputStream(fos);
        oos.writeObject(d);
        oos.writeObject(c);
-          
+       // Reading the objects
+       
+       
+       FileInputStream fis = new FileInputStream("serial2.ser");
+       ObjectInputStream ois = new ObjectInputStream(fis);
+       Dog d2 = (Dog)ois.readObject();
+       Cat c2 = (Cat)ois.readObject();
+       System.out.println(d2.id + " "+d2.name);
+       System.out.println(c2.id + " "+c2.name);
 
 
       
