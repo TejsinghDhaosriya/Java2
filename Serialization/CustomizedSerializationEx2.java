24c1bc9ffa5c82029906bb0de87fd950bf896324
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-04-15 21:36:28 +0530

    CustomizedSerialization Example 2nd with username pwd and pin

---
+++ Serialization/CustomizedSerializationEx2.java
@@ -0,0 +1,29 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.ObjectInputStream;
+
+class Account implements Serializable
+{
+    String username = "tej";
+    String pwd  = "ramu";
+    int pin = 1234;
+
+}
+
+class CustomizedSerializationEx2
+ {
+  public static void main(String[] args) throws Exception
+  {
+   Account ac   = new Account();
+   FileOutputStream fos = new FileOutputStream("CustomizedSerializationEx2.ser");
+   ObjectOutputStream oos = new ObjectOutputStream(fos);
+   oos.writeObject(ac);
+
+   FileInputStream fis = new FileInputStream("CustomizedSerializationEx2.ser");
+   ObjectInputStream ois = new ObjectInputStream(fis);
+   Account ac2 =(Account)ois.readObject();
+   System.out.println(ac2.username +" pwd is "+ac2.pwd +" pin is "+ac2.pin);
+   }
+ }
