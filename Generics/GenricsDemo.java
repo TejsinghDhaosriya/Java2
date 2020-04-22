b16eeb79f1fc27ed07f63af2b6e7124c8b680469
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-04-22 18:22:25 +0530

    Genrics

---
+++ Generics/GenricsDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class GenricsDemo {
+
+public static void main(String[] args)
+  {
+
+  ArrayList l = new ArrayList();
+  Collection c = new ArrayList();
+  Collection <String> cw = new ArrayList<String>();
+  Collection <String> cw2 = new ArrayList<int>();
+  
+  }
+
+
+
+}
