381177942d10c56f397629553ab1f4514df53166
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-04-29 11:46:40 +0530

    HashMap

---
+++ Collection Framework/Map/HashMapDemo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class HashMapDemo
+{
+    public static void main(String[] args){
+    HashMap m = new HashMap();
+    m.put(101,"Tejsingh");
+    m.put("pass",123);
+    m.put("live","indore");
+    System.out.println(m);
+
+
+
+    }}
