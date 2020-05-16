6c8e515ae42fe8d3e0fccb5f74bc82e898d44a9a
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-05-16 11:45:57 +0530

    TreeSet

---
+++ Collection Framework/Set/Treeset/TreesetDemo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import static java.lang.System.out;
+
+class TreesetDemo{
+   public static void main(String []args)
+    {
+     TreeSet t = new TreeSet();
+     t.add("a");
+     t.add("1");
+     t.add("5");
+     t.add("3");
+     t.add("0");
+     t.add("A");
+     t.add("b");
+    
+  out.println(t);
+    
+    }
+
+}
