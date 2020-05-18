e221884794b27d7031f028f1e9d31156054130c8
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-05-18 13:29:26 +0530

    Priority Queue

---
+++ Collection Framework/Queue/QueueDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class QueueDemo {
+public static void main(String[] args)
+ {
+  PriorityQueue pq=  new PriorityQueue();
+   pq.offer("a");
+   pq.offer("b");
+   //pq.offer(1);
+   pq.offer("2");
+
+   System.out.println(pq);
+
+ }
+    
+
+}
