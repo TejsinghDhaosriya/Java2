d93b58f443c0e68afb14b4a2ca5bd7df0af667c0
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-05-26 09:14:56 +0530

    Binary Search using Comparator

---
+++ Collection Framework/Search/BinarySearch2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import static java.lang.System.out;
+
+class Tej {
+   public static void main(String[] args)
+   {
+   ArrayList l = new ArrayList();
+   l.add('C');
+   l.add('F');
+   l.add('Q');
+   l.add('V');
+   l.add('B');
+   l.add('A');
+   l.add('z');
+   l.add('a');
+  out.println(l);
+  Collections.sort(l,new mycomp());
+  out.println(l);
+  out.println(Collections.binarySearch(l,'C',new mycomp()));
+  out.println(Collections.binarySearch(l,'D',new mycomp()));
+   }
+}
+
+class mycomp implements Comparator
+  {
+    public int compare(Object o1,Object o2)
+    {
+        Character i1=(Character)o1;
+        Character i2=(Character)o2;
+        return -i1.compareTo(i2);
+    }      
+  }
