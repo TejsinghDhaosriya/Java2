cc5cbc36b519e527b4db9679b289cbf81880f353
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-06-09 12:43:38 +0530

    TimeDataAPI3.java

---
+++ Java 8/TimeDataAPI3.java
@@ -0,0 +1,36 @@
+
+import java.time.*;
+import static java.lang.System.out;
+
+import java.time.*;
+public class TimeDataAPI3 {
+    public static void main(String[] args)
+  {
+   LocalDateTime date = LocalDateTime.now();
+  // out.println(date);
+   int dd = date.getDayOfMonth();
+   int mm = date.getMonthValue();
+   int yyyy= date.getYear();
+  // out.println(dd+"..."+mm+"..."+yyyy);
+  out.printf("%d-%d-%d",dd,mm,yyyy);
+  
+
+  int h = date.getHour();
+  int m = date.getMinute();
+  int s = date.getSecond();
+  int ns = date.getNano();
+  out.printf("%d:%d:%d:%d",h,m,s,ns);
+  out.printf("\n");
+
+  LocalDateTime dt = LocalDateTime.of(1999,Month.JULY,07,12,34);
+  out.println(dt);
+  
+  out.println(dt.plusMonths(8));
+  out.println(dt.minusMonths(3));
+
+
+
+
+
+  }
+}
