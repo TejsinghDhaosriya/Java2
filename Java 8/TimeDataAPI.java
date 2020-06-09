6727c8c45c681ccb4c9122af3d359dce1482d56d
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-06-09 11:55:24 +0530

    TimeDataAPI.java

---
+++ Java 8/TimeDataAPI.java
@@ -0,0 +1,12 @@
+import java.time.*;
+public class TimeDataAPI {
+    public static void main(String[] args)
+    {
+    LocalDate ld =  LocalDate.now();
+    System.out.println(ld);
+    
+    LocalTime lt =  LocalTime.now();
+    System.out.println(lt);
+    }
+
+}
